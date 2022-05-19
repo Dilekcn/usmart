6db22e976267b49290c4cbfcbabcdd1992efc800
Dilekcn <canturkdk@gmail.com>
2022-05-20 00:46:54 +0100

    Data fetched

--- usmart/src/App.js
+++ usmart/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import DataFetching from './DataFetching';
 
 function App() {
   return (
     <div className="App">
-     
+     <DataFetching/>
     </div>
   );
 }
